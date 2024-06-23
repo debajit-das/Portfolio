050ad88d1140d48a7820a66729fc33de27e2be57
Debajit Das <debas1118@gmail.com>
2024-06-23 22:19:23 +0530

    update navigation bar in mobile view

--- JS/app.js
+++ JS/app.js
@@ -1,68 +1,22 @@
-// ------------------------------------DARK MODE-----------------------------------------------------
-// let body = document.querySelector("body");
-// let header = document.querySelector("header");
-// let theme = document.querySelector("#theme");
-// // let name = document.querySelector("#name");
-// // let p = document.querySelector("p");
-// let h1 = document.querySelector("h1");
-// let scrollDown = document.querySelector(".scrollDown");
-// // let logo = document.querySelector(".logo");
-// let logo = document.getElementsByClassName("logo");
-// let currTheme = "light";
-// let currLogo = "lightLogo";
-// theme.addEventListener("click", () => {
-//   if (currTheme === "light") {
-//     currTheme = "dark";
-//     body.classList.add("dark");
-//     // name.classList.add("dark");
-//     header.classList.add("dark");
-//     h1.classList.add("dark");
-//     // scrollDown.classList.add("dark");
-//     // logo.classList.add("darkLogo");
-//     body.classList.remove("light");
-//     // name.classList.remove("light");
-//     header.classList.remove("light");
-//     // scrollDown.classList.remove("light");
-//     h1.classList.remove("light");
-//     // logo.classList.remove("lightLogo");
-//   } else {
-//     currTheme = "light";
-//     body.classList.add("light");
-//     // name.classList.add("light");
-//     // header.classList.add("light");
-//     // scrollDown.classList.add("light");
-//     h1.classList.add("light");
-//     // logo.classList.add("lightLogo");
-//     body.classList.remove("dark");
-//     // name.classList.remove("dark");
-//     header.classList.remove("dark");
-//     // scrollDown.classList.remove("dark");
-//     h1.classList.remove("dark");
-//     // logo.classList.remove("darkLogo");
-//   }
-// });
-// theme.addEventListener("click", () => {
-//   if (currLogo === "lightLogo") {
-//     currLogo = "darkLogo";
-//     logo.classList.add("darkLogo");
-//     logo.classList.remove("lightLogo");
-//   } else {
-//     currLogo = "lightLogo";
-//     logo.classList.add("lightLogo");
-//     logo.classList.remove("darkLogo");
-//   }
-// });
-
 // -----------------------------------------nevigation menu--------------------------------------
-const bar = document.querySelector("#bar");
-const rightbarLinks = document.querySelector(".rightbarLinks");
-const currDisplay = "none";
-bar.addEventListener("click", () => {
-  if (rightbarLinks.style.display === "none") {
-    rightbarLinks.style.display = "flex";
-    currDisplay = "flex";
-  } else {
-    rightbarLinks.style.display = "none";
-    currDisplay = "none";
-  }
+document.addEventListener("DOMContentLoaded", () => {
+  const bar = document.querySelector("#bar");
+  const rightbarLinks = document.querySelector(".rightbarLinks");
+  bar.addEventListener("click", () => {
+    event.stopPropagation();
+    if (rightbarLinks.style.display === "none") {
+      rightbarLinks.style.display = "flex";
+    } else {
+      rightbarLinks.style.display = "none";
+    }
+    document.addEventListener("click", () => {
+      rightbarLinks.style.display = "none";
+    });
+    document.addEventListener("click", () => {
+      rightbarLinks.style.display = "none";
+    });
+    rightbarLinks.addEventListener("click", () => {
+      event.stopPropagation();
+    });
+  });
 });
